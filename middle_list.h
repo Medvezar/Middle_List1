664f45fd85c1d167983623cc46b510f276f82979
Michael <77501487+Medvezar@users.noreply.github.com>
2022-01-20 12:39:27 +0300

    Update middle_list.h

--- middle_list.h
+++ middle_list.h
@@ -11,7 +11,7 @@ string itc_rmstrspc(string str);
 string itc_rmstrchar(string str, string str2);
 long itc_sumlst(const vector <int>& vect);
 long itc_sum_even_lst(const vector <int>& vect);
-long itc_sum_even_part_lst(const vector <long>& vect);
+long itc_sum_even_part_lst(const vector <int>& vect);
 void itc_odd_even_separator_lst(const vector <int>& vect, vector <int>& lst1, vector <int>& lst2);
 void itc_pos_neg_separator_lst(const vector <int>& vect, vector <int>& lst1, vector <int>& lst2, vector <int>& lst3);
 void itc_odd_even_analysis_lst(const vector <int>& vect);
